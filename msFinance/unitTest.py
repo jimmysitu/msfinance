6f8aad35ff7558db971419320244effa32a95ffc
Jimmy Situ <web@jimmystone.cn>
2023-10-03 16:10:15 +0800

    Add more test case

--- msFinance/unitTest.py
+++ msFinance/unitTest.py
@@ -5,7 +5,12 @@ import time
 import sys
 import stocks
 
-stock = stocks.StockBase()
-stock.get_income_statment('aapl', 'xnas')
+stock = stocks.StockBase(debug=True)
+stock.get_income_statement('aapl', 'xnas')
+stock.get_balance_sheet_statement('aapl', 'xnas')
+stock.get_cash_flow_statement('aapl', 'xnas')
 
-
+stock.get_growth('aapl', 'xnas')
+stock.get_operating_and_efficiency('aapl', 'xnas')
+stock.get_financial_health('aapl', 'xnas')
+stock.get_cash_flow('aapl', 'xnas')
