d824af04dc279b8f66e2f73080919cb11b680ba3
Jimmy Situ <web@jimmystone.cn>
2023-11-02 00:14:50 +0800

    Update test flow

--- tests/test_stocks.py
+++ tests/test_stocks.py
@@ -9,30 +9,31 @@ import pandas as pd
 import requests
 import json
 
+def test_stocks():
+    proxy = 'socks5://127.0.0.1:1088'
 
-proxy = 'socks5://127.0.0.1:1088'
-
-stock = stocks.Stock(
-    debug=False,
-    session='/tmp/msfinance/msf.sql3',
-    proxy=None,
-)
+    stock = stocks.Stock(
+        debug=False,
+        session='/tmp/msfinance/msf.sql3',
+        proxy=None,
+    )
 
 
-tickers_list = {}
-tickers_list['xnas'] = stock.get_xnas_tickers()
-tickers_list['xnys'] = stock.get_xnys_tickers()
-tickers_list['xase'] = stock.get_xase_tickers()
+    tickers_list = {}
+    tickers_list['xnas'] = stock.get_xnas_tickers()
+    tickers_list['xnys'] = stock.get_xnys_tickers()
+    tickers_list['xase'] = stock.get_xase_tickers()
 
-sp500_tickers = stock.get_sp500_tickers()
+    sp500_tickers = stock.get_sp500_tickers()
 
-# Test method in class Stock
-stock.get_income_statement('aapl', 'xnas')
-stock.get_balance_sheet_statement('aapl', 'xnas')
-stock.get_cash_flow_statement('aapl', 'xnas')
+    # Test method in class Stock
+    stock.get_income_statement('aapl', 'xnas')
+    stock.get_balance_sheet_statement('aapl', 'xnas')
+    stock.get_cash_flow_statement('aapl', 'xnas')
 
-stock.get_growth('aapl', 'xnas')
-stock.get_operating_and_efficiency('aapl', 'xnas')
-stock.get_financial_health('aapl', 'xnas')
-stock.get_cash_flow('aapl', 'xnas')
+    stock.get_growth('aapl', 'xnas')
+    stock.get_operating_and_efficiency('aapl', 'xnas')
+    stock.get_financial_health('aapl', 'xnas')
+    stock.get_cash_flow('aapl', 'xnas')
+
 
